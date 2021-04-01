07a8615b322e3bf64a7f5bc23f205940c915bfa5
David Dansby <ddansby89@gmail.com>
2021-04-01 02:23:11 -0700

    Refactor app organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,23 +49,3 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		log.Println("create new snippet request:", err)
 	}
 }
-
-func main() {
-	// Use the http.NewServeMux() func to tinit a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-
-	log.Println("Starting server on localhost:4000")
-	if err := http.ListenAndServe(":4000", mux); err != nil {
-		log.Fatal(err)
-	}
-}
