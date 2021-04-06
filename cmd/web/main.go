99a3abdcd37d208393763adc8836ca1443db7360
David Dansby <ddansby89@gmail.com>
2021-04-06 00:49:00 -0700

    Add base to serve static ui files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,16 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a file server which serves files out of the "./ui/static" dir.
+	// Note that the path given to the http.Dir func is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() func to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
