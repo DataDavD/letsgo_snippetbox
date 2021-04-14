438501ffb277ae3ee8ad287e9827d0ab1e252999
David Dansby <ddansby89@gmail.com>
2021-04-14 03:43:10 -0700

    Refactor main to handle routes separately

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -26,23 +26,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Use the http.NewServeMux() func to tinit a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server which serves files out of the "./ui/static" dir.
-	// Note that the path given to the http.Dir func is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	// Use the mux.Handle() func to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
@@ -50,7 +33,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(), // Call the new app.routes() method
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
---
+++ cmd/web/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	return mux
+}
