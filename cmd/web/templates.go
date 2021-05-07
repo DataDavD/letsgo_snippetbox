04c25d5c19ba9cd0f1afff93127d6b640e241563
David Dansby <ddansby89@gmail.com>
2021-05-07 15:44:51 -0700

    Make custom template human date readable function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/DataDavD/snippetbox/pkg/models"
 )
@@ -15,6 +16,18 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+// humanDate returns a nicely formatted human-readable string representation of time.Time.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is essentially
+// a string-keyed map which acts as a lookup between the names of our custom template
+// functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -35,8 +48,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		// file path and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Parse the page template file into a template set.
-		ts, err := template.ParseFiles(page)
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
