25c65d40a355ce9664156460e0e57a83a8d771ff
David Dansby <ddansby89@gmail.com>
2021-05-16 18:37:21 -0700

    Chain middleware using justinas/alice 3rd party pkg
    This commit adds justinas/alice to go modules files and implements the
    alice middleware chaining for better readability of our middleware.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard" middleware
+	// which will be used for every request our app receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -12,5 +20,5 @@ func (app *application) routes() http.Handler {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/DataDavD/snippetbox
 
 go 1.15
 
-require github.com/go-sql-driver/mysql v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/justinas/alice v1.2.0
+)
