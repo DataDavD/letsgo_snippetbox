bde513a7dad37631ccd9858524d8dfeea7811098
David Dansby <ddansby89@gmail.com>
2021-05-16 17:32:14 -0700

    Create middleware to add XSS security headers for every request
    This commit creates middleware to add X-Frame-Options and
    X-XSS-Protection headers. We implement this middleware in front of the
    the servemux so that it acts on every request.

---
+++ cmd/web/middleware.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -10,5 +10,9 @@ func (app *application) routes() *http.ServeMux {
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
+
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because the secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else
+	return secureHeaders(mux)
 }
