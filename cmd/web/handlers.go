4d3221d3870681a9d090c4c8e31b5ba07ecb543e
David Dansby <ddansby89@gmail.com>
2021-06-22 23:04:20 -0700

    Make placeholder functions for new user authentication handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -106,3 +106,23 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Redirect the user to the relevant page for the created snippet
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
+
+func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display the user signup form...")
+}
+
+func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+
+func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display the user login form...")
+}
+
+func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+
+func (app application) logoutUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
