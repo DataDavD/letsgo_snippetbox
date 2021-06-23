a28c901a451548e3aa2bb656bcd6c1fd8b91de5c
David Dansby <ddansby89@gmail.com>
2021-06-22 23:57:33 -0700

    Add User model and 2 new errors for invalid credentials and dupe email

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,17 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+
+	// ErrInvalidCredentials error is used if a user tries
+	// to login with an incorrect email address or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	// ErrDuplicateEmail error is used if a user tries to signup
+	// with an email address that's already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 type Snippet struct {
 	ID      int
@@ -13,4 +23,13 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+	Active         bool
 }
