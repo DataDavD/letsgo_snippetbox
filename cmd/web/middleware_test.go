aa7211a08b7dc60db145aba8becb3f3e72b40965
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-09-01 00:56:40 -0700

    Add test for secureHeaders middleware
    This commit adds the TestSecureHeaders test for testing the
    secureHeaders middleware. Specifically, we test that the middleware
    correctly adds the appropriate security headers we expect with the
    middleware.
    
    Furthermore, we also ensure that the middleware correctly
    calls the next handler in the middleware chain. We do this by using a
    mock HTTP handler that writes "OK" to the response body. We test that
    the middleware calls the next handler in the chain by ensuring that
    we get the expected 200 status code and "OK" response body.

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	// Initialize a new httptest.ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// Initialize a dummy http.Request.
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a mock HTTP handler that we can pass to our secureHeaders middleware,
+	// which writes a 200 status code and "OK" response body
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("OK")); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	// Pass the mock HTTP handler to our secureHeaders middleware. Because secureHeaders
+	// *returns* a http.Handler we call its ServeHTTP() method, passing in the
+	// http.ResponseRecorder and dummy http.Request to execute it.
+	secureHeaders(next).ServeHTTP(rr, r)
+
+	// Call the Result() method on the http.ResponseRecorder to get the results of the test.
+	rs := rr.Result()
+
+	// Check that the middleware has correctly set the X-Frame-Options header on the response.
+	frameOptions := rs.Header.Get("X-Frame-Options")
+	if frameOptions != "deny" {
+		t.Errorf("want %q; got %q", "deny", frameOptions)
+	}
+
+	// Check that the middleware has correctly set the X-XSS-Protection header on the response.
+	xssProtection := rs.Header.Get("X-XSS-Protection")
+	if xssProtection != "1; mode=block" {
+		t.Errorf("want %q; got %q", "1; mode=block", xssProtection)
+	}
+
+	// Check that the middleware has correctly called the next handler in line and that
+	// the response status code and body are as expected.
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	defer func() {
+		if err := rs.Body.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
