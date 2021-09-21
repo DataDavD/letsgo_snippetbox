22ecc24f8a4bfdbbc526d3241caf31f507ba8660
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-09-21 00:12:43 -0700

    Refactor application struct
    This commit refactors the application struct so that it's snippets and
    users fields are defined as interfaces so that we can make use of our
    mocked version of snippets and users in testing with the application
    struct.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/DataDavD/snippetbox/pkg/models"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golangcollege/sessions"
 
@@ -22,12 +23,20 @@ type contextKey string
 const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
