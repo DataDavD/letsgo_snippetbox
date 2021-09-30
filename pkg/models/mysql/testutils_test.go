930deef46aed2b436d574a0b3f4bd1022b47b17c
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-09-30 01:53:10 -0700

    Create sql test utilities
    This commit creates setup and teardown SQL scripts to ensure we use a
    new test DB for each integration test and ensure we tear it down after
    each integration test. The scripts are utilized in the newTestDB
    constructor which connects to the test database, runs the setup SQL
    script, and then returns an anonymous teardown function to be run
    defer'd when executed during testing.

---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,52 @@
+package mysql
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	// Establish a sql.DB connection pool for our test database. Because our setup and teardown
+	// scripts contains multiple SQL statements, we need to use the 'multiStatements=true' parameter
+	// in our DSN. This instructs our MySQL database driver to support executing multiple SQL
+	// statements in one db.Exec() call.
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the setup SQL script from file and execute the statements.
+	script, err := os.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Return the connection pool and an anonymous function which reads and executs the teardown
+	// script, and closes the connection pool. We can assign this anonymous function and call
+	// it later once our test has completed.
+	return db, func() {
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer func() {
+			err := db.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+		}()
+	}
+
+}
